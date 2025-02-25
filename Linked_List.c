d7d5783bf78daef9db8c16166e8efe4941cacfcf
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-02-25 14:15:09 -0500

    Notes on Linked List

---
+++ Linked_List.c
@@ -0,0 +1,40 @@
+/*
+Author: Donald Jackson
+File: Linked_List.c
+Description: Linked List implementation and example usage.
+
+
+Notes:
+             -- A linked list is a data structure which lets you efficiently insert and delete arrays. 
+             -- Linked Lists are used to implement Stacks, Queues, and Deques
+             
+
+            Characteristics:
+             -- Non-Contiguous: Elements may be not touching or connected. I.E. there are multiple blocks of memory at different addresses, 
+                                Head Node (Data, Link) - > Node 1 (Data, Link) -> Node N (Data, Link) -> NULL
+            -- Accessed Sequentially, 
+
+            Types of Linked Lists:
+             -- Doubly Linked List
+             -- Singly Linked List
+             -- Circular Linked List
+             
+             
+            Typical Operations:
+             -- Length
+             -- Print
+             -- Insert
+             -- Search
+             -- Delete Key
+             -- Delete Given Position
+             -- Delete List
+             -- Get Nth Node from Start
+             -- Get Nth Node from End
+             -- Size of Doubly Linked List
+*/
+
+
+// Singly Linked List
+
+
+
