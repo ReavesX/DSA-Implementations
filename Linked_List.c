2ce4122bd5607d2ec1b9d13015f88d6d04502d3e
Donald Jackson <103725783+ReavesX@users.noreply.github.com>
2025-02-25 14:42:46 -0500

    created methods for inserting a value within the linked list and printing the linked list youve created.

--- Linked_List.c
+++ Linked_List.c
@@ -37,4 +37,44 @@ Notes:
 // Singly Linked List
 
 
+struct Node{
+    int data;  // This is the Data element of the node
+    struct Node* link; // This is where the next node is located
+};
 
+
+// This is how you create a node. 
+struct Node* createNode(int data){
+    struct Node* temp = (struct Node*)malloc(sizeof(struct Node));  // allocate memory for the node & typecast it as a Node
+    temp->data = data; // store memory for data integer in the memory allocated for the struct
+    temp->link = NULL; // stores the value of the link as null (this is the last link of the node)
+    return temp;
+}
+
+void insertValue(int data){
+    Node* temp = (struct Node*)malloc(sizeof(struct Node));  // allocate memory for the node & typecast it as a Node
+    (*temp).data = value;
+    temp->link = NULL;
+    head = temp;
+    
+}
+void printLinkedList();
+
+// implementation basic: 
+
+struct Node* head; // the head is global and can be accessed everywhere.
+
+int main(){
+    head = NULL
+    printf("how many numbers are there?")
+    int amount, i , value;
+    scanf("%d",&amount); // Get input for how many numbers there are in the list
+
+    for(i = 0; i < amount; i++){ // Loops from 0 until the amount of numbers that the user specified
+                                 // inserting the number given into the linked list
+        printf("Enter the number:\n");
+        scanf("%d", &value);
+        insertValue(value);
+        printLinkedList();
+    }
+}
